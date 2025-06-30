723be36442e0ed962787a9485b5a861e8cc7c705
Victor <vpongnian@medici.tv>
2025-06-30 12:00:41 +0200

    quick fix

--- api_lambda/api.tf
+++ api_lambda/api.tf
@@ -34,7 +34,7 @@ resource "aws_apigatewayv2_route" "api_lambda_route_auth" {
   count              = var.api_authorization_type == "CUSTOM" ? 1 : 0
   api_id             = var.api_gw_id
   route_key          = var.api_route_key
-  authorization_type = var.authorization_type
+  authorization_type = var.api_authorization_type
   authorizer_id      = var.api_authorizer_id
   target             = "integrations/${aws_apigatewayv2_integration.api_lambda_integration.id}"
 }
