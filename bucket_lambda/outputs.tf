ded0b282e4ab7bcb3c12733768720f46c4cb0828
Victor <vpongnian@medici.tv>
2026-01-07 12:06:07 +0100

    fix output bucket_lambda

--- bucket_lambda/outputs.tf
+++ bucket_lambda/outputs.tf
@@ -1,7 +1,7 @@
 output "bucket_lambda_arn" {
-  value = module.data-automation_bucket_lambda.lambda_function_arn
+  value = module.bucket_lambda.lambda_function_arn
 }
 
 output "bucket_lambda_name" {
-  value = module.data-automation_bucket_lambda.lambda_function_name
+  value = module.bucket_lambda.lambda_function_name
 }
