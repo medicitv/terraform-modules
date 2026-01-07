13481404a7415d51401518b6ef4678e073aee069
Victor <vpongnian@medici.tv>
2026-01-07 11:51:26 +0100

    add bucket_lambda

---
+++ bucket_lambda/outputs.tf
@@ -0,0 +1,7 @@
+output "bucket_lambda_arn" {
+  value = module.data-automation_bucket_lambda.lambda_function_arn
+}
+
+output "bucket_lambda_name" {
+  value = module.data-automation_bucket_lambda.lambda_function_name
+}
---
+++ bucket_lambda/variables.tf
@@ -0,0 +1,72 @@
+variable "name" {}
+variable "description" {}
+variable "package_path" {
+    type = string
+    default = ""
+}
+variable "package_type" {
+    type = string
+    default = "Zip"
+}
+variable "handler" {}
+variable "policy_document" {}
+variable "runtime" {
+    type = string
+    default = "python3.12"
+}
+variable "memory_size" {
+    type        = number
+    default     = 2048
+}
+variable "timeout" {
+    type        = number
+    default     = 600
+}
+variable "image_uri" {
+    type = string
+    default = null
+}
+variable "use_existing_cloudwatch_log_group" {
+    type = bool
+    default = false
+}
+variable "reserved_concurrent_executions" {
+    type        = number
+    default     = -1
+}
+variable "layers" {
+    type        = list(string)
+    default     = []
+}
+variable "environment_variables" {
+    type        = map(string)
+}
+variable "vpc_security_group_ids" {
+    type        = list(string)
+}
+variable "vpc_subnet_ids" {}
+
+variable "ignore_source_code_hash" {
+    type = bool
+    default = false
+}
+variable "cloudwatch_log_group_retention_in_days" {
+  description = "Specifies the number of days you want to retain log events in log group for Lambda."
+  type        = number
+  default     = 0
+}
+variable "cloudwatch_log_group_tags" {
+  description = "Additional tags for the Cloudwatch log group"
+  type        = map(string)
+  default     = {}
+}
+variable "TAGS" {
+    type        = map(string)
+}
+
+variable "bucket_notification_arn" {}
+variable "bucket_notification_id" {}
+
+variable "notification_events" {
+    type = list(string)
+}
